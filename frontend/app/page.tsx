d48ecc7575298ead7d1cfa32b10506ab64ac279a
Gustavo <kg.2k.pl@gmail.com>
2024-08-26 10:26:25 -0400

    feature: create-banner

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,8 +5,18 @@ import { Banner } from "@/components/banner";
 
 export default function Home() {
   return (
+    <>
     <main className="flex  items-center justify-between p-24">
-      <Banner num={1}/>
+      <div>PAGINA PRINCIPAL</div>
+
+      <Link href="/login">
+        <Button type="primary">IR A LA PAGINA DE LOGIN</Button>
+      </Link>
+      <Link href="/portal">
+        <Button type="primary">HOME</Button>
+      </Link>
     </main>
+    <Banner/>
+    </>
   );
 }
--- frontend/components/banner.tsx
+++ frontend/components/banner.tsx
@@ -1,31 +1,22 @@
-"use client"
-import { Button } from 'antd'
-import Link from 'next/link'
-import { useState } from 'react'
+import Link from "next/link"
 
-interface Props {
-    num: number
-}
-export const Banner = ({num}: Props) => {
-
-    
-
-    const [count, setCount] = useState(num)
+export const Banner = () => {
 
     return (
         <>
-            <div className="w-full h-64 bg-gray-200">
-                <h1>Titulo</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Quaerat sunt ad voluptatum quasi itaque sed nobis doloremque distinctio,
-                    assumenda harum quidem officiis iusto, fugiat, aperiam enim dicta tempora
-                    facilis. Repellat?</p>
-                <Link href="/login">
-                    <Button type="primary">Login</Button>
-                </Link>
+            <div className="w-full h-64 flex items-center justify-between p-20">
+                <div className="w-1/2">
+                    <h1 className="text-6xl mb-6">¡No dejes pasar esta oportunidad!</h1>
+                    <p className="text-2xl mb-12"> Puedes cumplir tus sueños de tener tu casa propia, adquirir un vehículo y/o empezar un negocio con nuestros Bienes Adjudicados.</p>
+                    <Link href="#">
+                        <button className="w-2/3 h-10 bg-green-800 border-green-800 text-white rounded">Consulta sobre tus Bienes &rarr;</button>
+                    </Link>
+                </div>
+                <div>
+                    <img src="https://www.ultracreditos.com/app/images/logos-bancos/logo-bmsc-wide-500.png" alt="banner" 
+                        className="w-100 h-100 rounded-3xl"></img>
+                </div>
             </div>
-            <p>Contador: {count}</p>
-            <button onClick={() => setCount(count + 1)}>+</button>
         </>
     )
 }
