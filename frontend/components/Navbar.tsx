c5c4f651c17f58839669424def01ecfb2f842284
“pedroarias <pedroariasvilela@gmail.com>
2024-08-27 13:14:18 -0500

    change : logo and docker compose

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -5,6 +5,7 @@ import PrimaryButton from './PrimaryButton'
 import { itemsNavbarB, itemsNavbarA } from '@/configs/items-navbar'
 import Image from 'next/image'
 import { AlignRightOutlined, ArrowRightOutlined, CloseOutlined } from '@ant-design/icons'
+import Logo from "../public/logo.svg"
 
 export default function Navbar() {
 
@@ -16,17 +17,19 @@ export default function Navbar() {
 
     return (
         <div className="w-full flex">
-            <Image
-                src="https://www.bmsc.com.bo/_nuxt/img/header-logo.96143ea.svg"
-                alt="Picture of the author"
-                width={350}
-                height={350}
-                className="w-1/2 sm:w-1/3 md:w-1/4 "
-            />
+            <div className="w-[30%] lg:w-[20%] flex justify-center ml-5">
+                <Image
+                    src={Logo}
+                    alt="Picture of the author"
+                    width={250}
+                    height={250}
+                    className="w-full sm:w-1/2 md:w-1/2 lg:w-full lg:h-[150px] "
+                />
+            </div>
 
-            <div className="w-[80%] flex-col items-end pr-3 hidden lg:flex">
+            <div className="w-[80%] flex-col items-end pr-3 hidden lg:flex justify-center">
                 <div className="w-[95%] justify-end flex p-2 gap-3 items-center border-b">
-                    <Link className="px-4 text-sm text-green-700 font-semibold cursor-pointer" href="">Personas</Link>
+                    <Link className="px-4 text-sm text-blue-900 font-semibold cursor-pointer" href="">Personas</Link>
                     {itemsNavbarA.map((item, index) => (
                         <Link key={index} className="px-4 text-sm font-semibold cursor-pointer" href="">{item.name}</Link>
                     ))}
@@ -40,17 +43,17 @@ export default function Navbar() {
                 </div>
             </div>
 
-            <AlignRightOutlined onClick={toogleOpen} className="text-green-900 text-3xl block lg:hidden ml-auto m-4" />
+            <AlignRightOutlined onClick={toogleOpen} className="text-blue-900 text-3xl block lg:hidden ml-auto m-4" />
 
             {isMenuOpen && (
                 <div className="absolute left-0 w-full h-screen bg-white flex flex-col xl:hidden lg:hidden md:hidden z-40 pb-3 shadow-md shadow-slate-900">
-                    <CloseOutlined onClick={toogleOpen} className='text-green-900 text-3xl block lg:hidden ml-auto m-4' />
+                    <CloseOutlined onClick={toogleOpen} className='text-blue-900 text-3xl block lg:hidden ml-auto m-4' />
                     <div className="flex flex-col justify-start text-start px-4">
-                        {itemsNavbarA.map((item, index)=>(
-                        <Link key={index} href={""} className="py-4 text-md border-b font-semibold cursor-pointer flex justify-between">
-                             <h1>{item.name}</h1>
-                             <ArrowRightOutlined />
-                        </Link>
+                        {itemsNavbarA.map((item, index) => (
+                            <Link key={index} href={""} className="py-4 text-md border-b font-semibold cursor-pointer flex justify-between">
+                                <h1>{item.name}</h1>
+                                <ArrowRightOutlined />
+                            </Link>
                         ))}
                         <Link className="py-4 text-md text-green-700 font-semibold cursor-pointer" href="">Banca 24/7</Link>
                         <Link className="py-4 text-md text-green-700 font-semibold cursor-pointer" href="">Contactanos</Link>
