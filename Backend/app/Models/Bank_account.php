4407ba297b0efcbcc28edb59970489422a6dd0ac
alexisRod <alexisjoserodriguezriveo@gmail.com>
2024-08-27 09:39:25 -0300

    seeders de registros iniciales

--- Backend/app/Models/Bank_account.php
+++ Backend/app/Models/Bank_account.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Type_user extends Model
+class Bank_account extends Model
 {
     use HasFactory;
-    protected $table = "type_user";
 }
