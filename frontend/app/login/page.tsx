4bbfdb949db7004ce4be31cb04bce6f828763617
Gustavo <kg.2k.pl@gmail.com>
2024-09-05 16:32:12 -0400

    fix

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -12,7 +12,7 @@ export default function Login() {
                 style={{ backgroundImage: 'url(https://app.fintech.com/static/c19/login_bg.svg)', pointerEvents: 'none' }} />
             <h1 className="text-4xl font-bold text-white mb-8">InnovaBank</h1>
             <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-                <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+                <h1 className="text-2xl font-bold mb-6 text-center">Inicio de sesión</h1>
                 <Form
                     name="login"
                     className="space-y-4"
@@ -25,7 +25,7 @@ export default function Login() {
                         rules={[{ required: true, message: 'Por favor ingrese su nombre de usuario!' }]}>
                         <Input
                             prefix={<UserOutlined />}
-                            placeholder="Username"
+                            placeholder="Nombre de usuario"
                             className="border-gray-300 rounded-lg" />
                     </Form.Item>
                     <Form.Item
@@ -33,24 +33,16 @@ export default function Login() {
                         rules={[{ required: true, message: 'Por favor ingrese su contraseña!' }]}>
                         <Input.Password
                             prefix={<LockOutlined />}
-                            placeholder="Password"
+                            placeholder="Contraseña"
                             className="border-gray-300 rounded-lg" />
                     </Form.Item>
                     <Form.Item className='flex justify-center items-center'>
-                        <PrimaryButton extendClassName='px-24' label={"Log in"} />
+                        <PrimaryButton extendClassName='px-24' label={"Iniciar sesión"} />
                     </Form.Item>
                 </Form>
-                <div className="mt-4 flex justify-between">
-                    <Link href="/portal" className="text-blue-500 hover:underline">
-                        Go to Portal
-                    </Link>
-                    <Link href="/" className="text-blue-500 hover:underline">
-                        Home
-                    </Link>
-                </div>
             </div>
             <p className='text-white mt-4'>No tienes una cuenta? 
-                <Link href="/register" className="text-blue-500 text-white mt-4 underline ml-1">Registrate</Link>
+                <Link href="/register" className="text-blue-500 text-white mt-4 underline ml-1">Regístrate</Link>
             </p>
         </div>
     );
