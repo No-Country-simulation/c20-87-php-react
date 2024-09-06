5c16879ed52eb2f1629f92b1041961a3a39589b8
danielpyrod <145179127+danielpyrod@users.noreply.github.com>
2024-09-05 21:18:16 -0400

    Listar balance con type user admin incluido

--- Backend/app/Http/Controllers/BalanceController.php
+++ Backend/app/Http/Controllers/BalanceController.php
@@ -2,23 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BankAccount;
+use App\Models\Bank_account;
 use Illuminate\Http\Request;
 
 class BalanceController extends Controller
 {
     
     // Metodo para listar todos los balances
-    public function listAllBalances(Request $request){
+    public function listAllBalances(Request $request)
+{
+    // Verificación de autenticación de usuario
+    if (!auth()->check()) {
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
 
-        // Autenticación por defecto de usuario
-        if (!auth()->check()) {
-            return response()->json(['message' => 'Unauthorized'], 401);
-        }
-        $userId = auth()->id();
+    // Verificación del tipo de usuario (solo para usuarios con type_user = 3)
+    $user = auth()->user();
+    if ($user->type_user !== 3) {
+        return response()->json(['message' => 'Forbidden: Access denied'], 403);
+    }
 
-        // Tomamos los datos de los modelos BankAccount y User
-        $allBalance = BankAccount::with('user:id,name,lastname') // Relación con el modelo User, seleccionando id y username
+    // Continuar con la consulta de balances si el usuario es type_user = 3
+    $allBalance = Bank_account::with('user:id,name,lastname') // Relación con el modelo User, seleccionando id y username
         ->select('user_id', 'account_number', 'balance') // Incluimos user_id
         ->get()
         ->map(function ($bankAccount) {
@@ -26,16 +31,14 @@ class BalanceController extends Controller
                 'user_id' => $bankAccount->user_id,
                 'account_number' => $bankAccount->account_number,
                 'name' => $bankAccount->user->name,
-                'lastname' => $bankAccount->user->lastname, 
+                'lastname' => $bankAccount->user->lastname,
                 'balance' => $bankAccount->balance,
-                
             ];
         });
 
     // Retornar todos los atributos de balance a JSON
-    return response()->json($allBalance); 
-
-    }
+    return response()->json($allBalance);
+}
 
 
 }
