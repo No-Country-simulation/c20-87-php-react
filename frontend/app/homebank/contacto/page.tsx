e396c747aac53e296cc2f8163423d72c3a801468
ivanng01 <ivann-gonzalez@hotmail.com>
2024-09-19 14:10:57 -0300

    creacion modulo contacto

---
+++ frontend/app/homebank/contacto/page.tsx
@@ -0,0 +1,9 @@
+import ContactLayout from '@/components/ContactLayout';
+
+export default function ContactPage() {
+    return (
+        <ContactLayout>
+            
+        </ContactLayout>
+    );
+}
